73c1970f9160fe6008384e75b858b61d5ffc680f
DarkCaster <dark.caster@outlook.com>
2016-11-01 02:34:11 +0300

    Added iscsi-connect and iscsi-disconnect scripts: simple scripts to perform connect\disconnect to configured iscsi target

---
+++ ISCSICryptTools/iscsi-disconnect.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#script_dir=`dirname "$0"`
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+show_usage () {
+ echo "usage: iscsi-disconnect.sh <config file, optionally relative to config dir>"
+ exit 100
+}
+
+log () {
+ local msg="$@"
+ echo "$msg"
+}
+
+check_errors () {
+ local status="$?"
+ local msg="$@"
+ if [ "$status" != "0" ]; then
+  if [ "z$msg" != "z" ]; then
+   log "$msg"
+  else
+   log "ERROR: last operation finished with error code $status"
+  fi
+  exit $status
+ fi
+}
+
+config="$@"
+test -z "$config" && show_usage
+
+test ! -f "$config" && config="$script_dir/config/$config"
+test -f "$config"
+check_errors "config file is missing"
+
+. "$config"
+check_errors "error while sourcing config file"
+
+test -z "$portal" && log "portal variable not set" && exit 100
+test -z "$iqn" && log "iqn variable not set" && exit 100
+
+log "performing logout"
+iscsiadm --mode node --targetname $iqn --portal $portal --logout
+check_errors
+
