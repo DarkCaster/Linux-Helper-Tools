5bb68069d30a334656bef4034ce1c511b46b2247
DarkCaster <dark.caster@outlook.com>
2016-12-02 04:46:02 +0300

    bash-lua-helper: example.sh - include lua-helper.sh.in

--- BashLuaHelper/example.sh
+++ BashLuaHelper/example.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
+# all paths may be relative to current dir
+. "$script_dir/lua-helper.sh.in" "$script_dir/example.cfg.lua" -e "config.sub" \
+-e "config.paths" -e "config.empty" -b "$script_dir/example.pre.lua" -a "$script_dir/example.post.lua" -o test1 -o test2 -w /tmp
+# ^ options at this line is optional ^
