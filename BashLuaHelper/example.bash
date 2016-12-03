bf76e4a13980c17eea010f18834b7494ae1b6667
DarkCaster <dark.caster@outlook.com>
2016-12-03 04:14:21 +0300

    bash-lua-helper: minor fixes, lua-helper.bash.in - env cleanup on exit

--- BashLuaHelper/example.bash
+++ BashLuaHelper/example.bash
@@ -6,7 +6,7 @@ echo "example.bash says: sourcing lua-helper.bash.in"
 
 # all paths may be relative to current dir
 . "$script_dir/lua-helper.bash.in" "$script_dir/example.cfg.lua" -e "config.sub" \
--e "config.paths" -e "config.empty" -b "$script_dir/example.pre.lua" -a "$script_dir/example.post.lua" -o test1 -o test2 -w /tmp
+-e "config.paths" -e "config.empty" -r cfg -b "$script_dir/example.pre.lua" -a "$script_dir/example.post.lua" -o test1 -o test2 -w /tmp
 # ^ options at this line is optional ^
 
 echo "example.bash says: lua-helper.bash.in complete"
