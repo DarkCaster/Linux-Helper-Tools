61076ad9eb0f2b1eb26ae0ed5e2bd10af695c479
DarkCaster <dark.caster@outlook.com>
2016-12-04 01:53:03 +0300

    wine-launcher: build config structure updated, build.post.lua - asserts updated

--- WineLauncher/build.post.lua
+++ WineLauncher/build.post.lua
@@ -3,12 +3,15 @@
 build=profiles[profile]
 
 assert(type(build) == "table", "build profile is incorrect")
-assert(type(build.src_link) == "string", "src_link value is missing or invalid")
-assert(type(build.sign_link) == "nil" or type(build.sign_link) == "string", "src_link value is missing or invalid")
+assert(type(build.src) == "table", "src value is missing or invalid")
+assert(type(build.src.link) == "string", "src_link value is missing or invalid")
+assert(type(build.src.sign) == "nil" or type(build.src.sign) == "string", "src.sign value is missing or invalid")
+assert(build.src.type == "wget" or build.src.type == "local", "src_get value is incorrect")
+
 assert(type(build.build_seq) == "table", "build_seq value is missing or invalid")
 assert(type(build.build_seq.prepare) == "table", "build_seq.prepare value is missing or invalid")
 assert(type(build.build_seq.configure) == "table", "build_seq.configure value is missing or invalid")
 assert(type(build.build_seq.make) == "table", "build_seq.make value is missing or invalid")
 assert(type(build.build_seq.install) == "table", "build_seq.install value is missing or invalid")
-assert(build.src_get == "wget" or build.src_get == "local", "src_get value is incorrect")
+
 
--- WineLauncher/wine-build.cfg.lua
+++ WineLauncher/wine-build.cfg.lua
@@ -14,13 +14,16 @@ profiles=
 {
 	wine_185_suse421=
 	{
-		-- wget, local
-		src_get="wget",
-		src_link="https://dl.winehq.org/wine/source/1.8/wine-1.8.5.tar.bz2",
-		-- optional
-		sign_link="https://dl.winehq.org/wine/source/1.8/wine-1.8.5.tar.bz2.sign",
-		build_seq={ prepare = build_seq.prepare, make=build_seq.make, install=build_seq.install,
-			configure = { "./configure --without-capi --without-cms --without-coreaudio --without-cups --without-curses --without-hal" .. prefix_addon, 'echo "configure complete"' },
+		src=
+		{
+			-- wget, local
+			type="wget",
+			link="https://dl.winehq.org/wine/source/1.8/wine-1.8.5.tar.bz2",
+			-- optional
+			sign="https://dl.winehq.org/wine/source/1.8/wine-1.8.5.tar.bz2.sign",
+		},
+		build_seq={ prepare=build_seq.prepare, make=build_seq.make, install=build_seq.install,
+			configure={ "./configure --without-capi --without-cms --without-coreaudio --without-cups --without-curses --without-hal" .. prefix_addon, 'echo "configure complete"' },
 		},
 	},
 }
