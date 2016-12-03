04142e4b5859c97381361b05d3af88d946c7ceb3
DarkCaster <dark.caster@outlook.com>
2016-12-04 01:46:32 +0300

    wine-launcher: build config updated, build.post.lua - added more asserts

--- WineLauncher/build.post.lua
+++ WineLauncher/build.post.lua
@@ -2,6 +2,13 @@
 
 build=profiles[profile]
 
-assert(type(build) ~= "nil", "build profile is incorrect")
+assert(type(build) == "table", "build profile is incorrect")
+assert(type(build.src_link) == "string", "src_link value is missing or invalid")
+assert(type(build.sign_link) == "nil" or type(build.sign_link) == "string", "src_link value is missing or invalid")
+assert(type(build.build_seq) == "table", "build_seq value is missing or invalid")
+assert(type(build.build_seq.prepare) == "table", "build_seq.prepare value is missing or invalid")
+assert(type(build.build_seq.configure) == "table", "build_seq.configure value is missing or invalid")
+assert(type(build.build_seq.make) == "table", "build_seq.make value is missing or invalid")
+assert(type(build.build_seq.install) == "table", "build_seq.install value is missing or invalid")
 assert(build.src_get == "wget" or build.src_get == "local", "src_get value is incorrect")
 
--- WineLauncher/wine-build.cfg.lua
+++ WineLauncher/wine-build.cfg.lua
@@ -12,14 +12,16 @@ build_seq=
 
 profiles=
 {
-	wine_1_8_5=
+	wine_185_suse421=
 	{
 		-- wget, local
 		src_get="wget",
 		src_link="https://dl.winehq.org/wine/source/1.8/wine-1.8.5.tar.bz2",
 		-- optional
 		sign_link="https://dl.winehq.org/wine/source/1.8/wine-1.8.5.tar.bz2.sign",
-		build_seq=build_seq,
+		build_seq={ prepare = build_seq.prepare, make=build_seq.make, install=build_seq.install,
+			configure = { "./configure --without-capi --without-cms --without-coreaudio --without-cups --without-curses --without-hal" .. prefix_addon, 'echo "configure complete"' },
+		},
 	},
 }
 
