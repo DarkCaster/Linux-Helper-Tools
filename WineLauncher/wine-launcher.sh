4e26c25b9419c53c567c95fa029f44e5a0395aff
DarkCaster <dark.caster@outlook.com>
2016-12-05 02:49:42 +0300

    wine-launcher: added prefix.wine and prefix.root params handling

--- WineLauncher/wine-launcher.sh
+++ WineLauncher/wine-launcher.sh
@@ -27,4 +27,28 @@ check_errors () {
  fi
 }
 
+# echo "$cfg_list"
+
+#external wine distribution
+if check_lua_export prefix.wine; then
+ winedist="${cfg[prefix.wine]}"
+ test ! -d "$winedist" && log "directory $winedist is missing" && exit 1
+ winedist=`realpath "$winedist"`
+ test ! -d "$winedist" && log "directory $winedist is missing" && exit 1
+ export WINESERVER="$winedist/bin/wineserver"
+ export WINELOADER="$winedist/bin/wine"
+ export LD_LIBRARY_PATH="$winedist/lib64:$LD_LIBRARY_PATH"
+ export PATH="$winedist/bin:$PATH"
+fi
+
+if [ ! -d "${cfg[prefix.root]}" ]; then
+ log "creating ${cfg[prefix.root]} root dir for wine prefix"
+ mkdir -p "$wineroot"
+ check_errors
+fi
+
+wineroot=`realpath "${cfg[prefix.root]}"`
+test ! -d "$wineroot" && log "failed to transform ${cfg[prefix.root]} dir with realpath" && exit 1
+export WINEPREFIX="$wineroot"
+
 
