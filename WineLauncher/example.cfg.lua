208d3915f700d73076fed37211cedd8d5b50b719
DarkCaster <dark.caster@outlook.com>
2016-12-23 20:24:13 +0300

    wine-launcher: example.cfg.lua updated

--- WineLauncher/example.cfg.lua
+++ WineLauncher/example.cfg.lua
@@ -14,7 +14,7 @@ prefix =
 	lang = "en_US.UTF-8",	-- override current env lang settings.
 	-- wine = loader.workdir .. "winedist", -- path to wine installation. should be build and installed by wine-build.sh script
 	wine = "/mnt/data/Wine/wine_distribs/wine185_suse421",
-	docs = loader.workdir .. "docs", -- my documents folder(s) will be relinked to this path
+	docs = loader.path.combine(loader.workdir,"docs"), -- my documents folder(s) will be relinked to this path
 	-- optional parameters, some of them will be only applied on prefix create:
 	owner = "Penguinator", -- set owner
 	org = "Iceberg.inc", -- set company
@@ -22,9 +22,9 @@ prefix =
 	dll_overrides = -- dll's will be copied from user location to wine's system32 and overriden with selected rule
 	{
 		-- parameter format is { <override rule>, <dll source path>, <dll target file name without path>, [optional override name that entered to winecfg window, will be created automatically if missing] }
-		winemp3 = { "native", loader.workdir .. "l3codecx.acm", "winemp3.acm" },
-		imm32 = { "native", loader.workdir .. "imm32.dll", "imm32.dll" },
-		glu32 = { "builtin,native", loader.workdir .. "glu32.dll", "glu32.dll" },
+		winemp3 = { "native", loader.path.combine(loader.workdir,"extra","l3codecx.acm"), "winemp3.acm" },
+		imm32 = { "native", loader.path.combine(loader.workdir,"extra","imm32.dll"), "imm32.dll" },
+		glu32 = { "builtin,native", loader.path.combine(loader.workdir,"extra","glu32.dll"), "glu32.dll" },
 		-- if source or target filename is missing, override name must be present. so, rule will be created, but nothing will be copied to wineprefix
 		dnsapi = { "native,builtin", "", "", "dnsapi" },
 	},
