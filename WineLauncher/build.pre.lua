68348de3b05f61cae82c0dff7ab324bf85c6b637
DarkCaster <dark.caster@outlook.com>
2016-12-25 23:09:00 +0300

    wine-launcher: wine-build.sh updated, more service values are exported to lua scripts

--- WineLauncher/build.pre.lua
+++ WineLauncher/build.pre.lua
@@ -1,6 +1,9 @@
 -- service script, define some stuuf for use with main build config
 
-profile=loader.extra[1]
-prefix=loader.extra[2]
+self=loader.extra[1]
+profile=loader.extra[2]
+prefix=loader.extra[3]
+config=loader.extra[4]
+
 prefix_addon=string.format('--prefix="%s"', prefix)
 
--- WineLauncher/wine-build.sh
+++ WineLauncher/wine-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+curdir="$PWD"
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
-script_link=`readlink "$script_dir/$0"`
-test ! -z "$script_link" && script_dir=`realpath \`dirname "$script_link"\``
+self=`basename "$0"`
+test ! -e "$script_dir/$self" && echo "script_dir detection failed. cannot proceed!" && exit 1
+script_file=`readlink "$script_dir/$self"`
+test ! -z "$script_file" && script_dir=`realpath \`dirname "$script_file"\``
+test ! -z "$script_file" && self="$script_file" || self="$script_dir/$self"
 
 profile="$1"
 test -z "$profile" && echo "usage: wine-build <profile> <install prefix> [alternative wine-build.cfg.lua config file]" && exit 1
@@ -12,9 +16,10 @@ test -z "$prefix" && echo "usage: wine-build <profile> <install prefix> [alterna
 
 config="$3"
 test -z "$config" && config="$script_dir/wine-build.cfg.lua"
+config=`realpath -s "$config"`
 
 . "$script_dir/find-lua-helper.bash.in"
-. "$bash_lua_helper" "$config" -e build -b "$script_dir/build.pre.lua" -a "$script_dir/build.post.lua" -o "$profile" -o "$prefix"
+. "$bash_lua_helper" "$config" -e build -b "$script_dir/build.pre.lua" -a "$script_dir/build.post.lua" -o "$self" -o "$profile" -o "$prefix" -o "$config"
 
 # create tmpdir
 tmp_dir=`mktemp -d -t wine-build-XXXXXX`
