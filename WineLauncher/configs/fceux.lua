fbdb7f5d98807a80cbe02f2a6ea96c9c4c34ce7b
DarkCaster <dark.caster@outlook.com>
2016-12-25 05:57:10 +0300

    wine-launcher: added fceux example config file

---
+++ WineLauncher/configs/fceux.lua
@@ -0,0 +1,46 @@
+-- extract filename
+prefix =
+{
+	root = loader.path.combine(loader.workdir,"root"),
+	arch = "win32",
+	lang = "ru_RU.UTF-8",
+	wine = "/mnt/data/Wine/wine_distribs/wine200rc3_suse421",
+	docs = loader.path.combine(loader.workdir,"docs"),
+	owner = "Penguinator",
+	org = "Iceberg.inc",
+	menu = false,
+}
+
+tweaks =
+{
+	winetricks=loader.path.combine(launcher.dir,"extra","winetricks"),
+	allfonts=false,
+	fontsmooth="simple",
+}
+
+winecfg = { run = { "winecfg" } }
+
+shell = { run = { "bash" } }
+
+cmd = { run = { "wine cmd.exe" } }
+
+winetricks = { run = { tweaks.winetricks } }
+
+fceux =
+{
+	run =
+	{
+		"wine fceux.exe",
+		loader.path.combine(prefix.root,"drive_c","fceux")
+	},
+	desktop =
+	{
+		name = "FCEUX",
+		comment = "FCEUX NES emulator",
+		icon = loader.path.combine(loader.workdir,"fceux.png"),
+		categories = "Game;",
+		terminal = false,
+		startupnotify = false,
+	},
+}
+
