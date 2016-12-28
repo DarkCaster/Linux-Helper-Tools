13be79f70e0e26b7246b3e70bc2a59d3f9522e61
DarkCaster <dark.caster@outlook.com>
2016-12-29 02:09:57 +0300

    systemd-for-desktop: install and startup script updated, added service example (with required install section for auto start on DE login)

--- SystemdForDesktopUser/install.sh
+++ SystemdForDesktopUser/install.sh
@@ -4,7 +4,7 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 # main script. it will prepare systemd env
 # and start user services installed with "desktop.target"
-target_dir="$HOME/.bin"
+target_dir="$HOME/bin"
 mkdir -p "$target_dir"
 script="$target_dir/systemd-desktop-startup-script.sh"
 cp "$script_dir/systemd-desktop-startup-script.sh" "$script"
@@ -22,12 +22,7 @@ sed -i -e "s|__exec|$script|g" "$desktop_file"
 #user-service-directory
 unit_dir="$HOME/.config/systemd/user"
 mkdir -p "$unit_dir"
-
 cp "$script_dir/desktop.target"  "$unit_dir/desktop.target"
-cp "$script_dir/desktop.service" "$unit_dir/desktop.service"
-
-sed -i -e "s|__start|/bin/true|g" "$unit_dir/desktop.service"
-sed -i -e "s|__stop|/bin/false|g" "$unit_dir/desktop.service"
 
 systemctl --user daemon-reload
 
