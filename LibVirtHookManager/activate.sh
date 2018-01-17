41ac1cd88afe8ae76f2a7d4ba0b5c91f1102dfeb
DarkCaster <dark.caster@outlook.com>
2018-01-17 04:34:29 +0300

    LibVirtHookManager: activate.sh script updated, add .gitignore

--- LibVirtHookManager/activate.sh
+++ LibVirtHookManager/activate.sh
@@ -14,3 +14,8 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 mkdir -p "$target/hooks"
 ln -s "$script_dir/qemu-hook.sh" "$target/hooks/qemu"
 [[ ! -z "$cfg_dir" ]] && ln -s "$target/hooks/hook_manager" "$cfg_dir"
+[[ -z "$cfg_dir" ]] && cfg_dir="$target/hooks/qemu"
+
+# write config dir location
+2>/dev/null echo "$cfg_dir" > "/etc/qemu-hook-cfgdir" && exit 0 || true
+2>/dev/null echo "$cfg_dir" > "$script_dir/qemu-hook-cfgdir" && exit 0 || true
