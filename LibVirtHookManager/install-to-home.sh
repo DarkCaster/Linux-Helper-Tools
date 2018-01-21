49fe1523c7a1890a6f63868cdc932c96bb47f188
DarkCaster <dark.caster@outlook.com>
2018-01-22 02:36:17 +0300

    LibVirtHookManager: add install-to-home.sh script

---
+++ LibVirtHookManager/install-to-home.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install libvirt hook manager to local user home directory
+# You must run libvirt-hook-manager-activate after installation,
+# in order to install symlink to qemu-hook.sh into libvirt config dir
+# and register hook-config directory.
+# This directory will be used for user hook-config files activation,
+# and it must have write permissions for user that will run libvirt-hook-manager
+
+target="$1"
+[[ -z $target ]] && target="$HOME/libvirt_hook_manager"
+[[ $target = $HOME ]] && echo "will not proceed install to home directory directly, try $HOME/libvirt_hook_manager sub-directory instead" && exit 1
+
+curdir="$( cd "$( dirname "$0" )" && pwd )"
+
+set -e
+mkdir -p "$target"
+cp "$curdir"/*{.sh,.lua,.in} "$target"
+
+rm -f "$HOME/bin/libvirt-hook-manager"
+rm -f "$HOME/bin/libvirt-hook-manager-activate"
+
+ln -s "$target/libvirt-hook-manager.sh" "$HOME/bin/libvirt-hook-manager"
+ln -s "$target/activate.sh" "$HOME/bin/libvirt-hook-manager-activate"
