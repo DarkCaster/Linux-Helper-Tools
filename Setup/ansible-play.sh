baa4f9bb5adc248e8d6c0a77d2abc0941c8730bd
DarkCaster <dark.caster@outlook.com>
2018-01-24 18:37:26 +0300

    Setup: add ansible-play.sh script

---
+++ Setup/ansible-play.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+cd "$script_dir"
+
+target="$1"
+[[ -z $target ]] && echo "usage: ansible-play.sh <yml playbook>" && exit 1
+[[ ! -f $target ]] && echo "target playbook file is missing" && exit 1
+
+target_file=`realpath -e "$target"`
+target_dir=`dirname "$target_file"`
+cd "$target_dir"
+
+ansible-playbook --private-key="$script_dir/00_ansible_key/ssh_key" -i "$script_dir/ansible-inventory" "$target_file"
