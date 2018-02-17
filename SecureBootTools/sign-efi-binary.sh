96b850c866f4b174228be153bf8062b5d8ca28a3
DarkCaster <dark.caster@outlook.com>
2018-02-17 18:51:19 +0300

    secure-boot-tools: sign-efi-binary.sh script updated

--- SecureBootTools/sign-efi-binary.sh
+++ SecureBootTools/sign-efi-binary.sh
@@ -6,7 +6,7 @@ output="$2"
 #because pesign do not support non interactive operation with encrypted NSS db-storage,
 #shred util is mandatory to securely destroy unencrypted keys and NSS db that may be left on disk after script is complete.
 shred_bin=`which shred 2>/dev/null`
-test -z "$shred_bin" && echo "shred utility is missing, cannot continue" && exit 1
+[[ -z "$shred_bin" ]] && echo "shred utility is missing, cannot continue" && exit 1
 
 #script_dir=`dirname "$0"`
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
@@ -16,62 +16,37 @@ show_usage () {
  exit 100
 }
 
-test -z "$input" && show_usage
-test -z "$output" && show_usage
-test ! -d "$script_dir/keys" && echo "keys subdirectory is missing, run generate-signkeys.sh first" && exit 1
-
-log () {
- local msg="$@"
- echo "$msg"
-}
+[[ -z "$input" ]] && show_usage
+[[ -z "$output" ]] && show_usage
+[[ ! -d "$script_dir/keys" ]] && echo "keys subdirectory is missing, run generate-signkeys.sh first" && exit 1
 
 dbdir=`mktemp -d`
-log "using temp dir: $dbdir"
+echo "using temp dir: $dbdir"
+
+set -eE
 
 cleanup () {
- if [ ! -z "$dbdir" ] && [ -d "$dbdir" ]; then
-  log "cleaning up temp dir $dbdir"
-  "$shred_bin" "$dbdir"/*
+ if [[ ! -z "$dbdir" && -d "$dbdir" ]]; then
+  echo "cleaning up temp dir $dbdir"
+  "$shred_bin" "$dbdir"/* || true
   rm -rf "$dbdir"
  fi
 }
 
-check_errors () {
- local status="$?"
- local msg="$@"
- if [ "$status" != "0" ]; then
-  if [ "z$msg" != "z" ]; then
-   log "$msg"
-  else
-   log "ERROR: last operation finished with error code $status"
-  fi
-  cleanup
-  exit $status
- fi
-}
+trap 'cleanup' ERR EXIT
 
-test -z "$dbdir" && log "error creting temp directory" && exit 1
-test ! -d "$dbdir" && log "error creting temp directory" && exit 1
+test -z "$dbdir" && echo "error creting temp directory" && exit 1
+test ! -d "$dbdir" && echo "error creting temp directory" && exit 1
 
 #decrypt private key
-log "decrypting private key, and converting it to PKCS#12"
+echo "decrypting private key, and converting it to PKCS#12"
 openssl aes-256-cbc -d -a -in "$script_dir/keys/private.key.enc" | cat - "$script_dir/keys/public.crt" | openssl pkcs12 -export -passout pass: -name signcert -out "$dbdir/signcert.p12"
-check_errors
 
-log "generating NSS database"
+echo "generating NSS database"
 certutil -N --empty-password -d "$dbdir" -f /dev/stdin
-check_errors
 
-log "importing PKCS#12 sign cert"
+echo "importing PKCS#12 sign cert"
 pk12util -i "$dbdir/signcert.p12" -d "$dbdir" -W ""
-check_errors
 
-log "signing $input and saving it as $output"
+echo "signing $input and saving it as $output"
 pesign -s -n "$dbdir" -c signcert -i "$input" -o "$output"
-check_errors
-
-#cleanup
-cleanup
-
-exit 0
-
