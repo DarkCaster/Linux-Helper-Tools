4db670dea323d47185b451b6cd3669ce331c0ffd
DarkCaster <dark.caster@outlook.com>
2018-02-17 18:41:06 +0300

    secure-boot-tools: shim-install-portable.sh script updated

--- SecureBootTools/shim-install-portable.sh
+++ SecureBootTools/shim-install-portable.sh
@@ -15,6 +15,8 @@ grub_override_arch="y"
 #script_dir=`dirname "$0"`
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
+set -e
+
 #try to use locally extracted shim and grub rpm, from opensuse
 test -d "${script_dir}/local/shim" && echo "using local shim binaries" && source_dir_shim="${script_dir}/local/shim${source_dir_shim}"
 test -d "${script_dir}/local/grub" && echo "using local grub binaries" && source_dir_grub="${script_dir}/local/grub${source_dir_grub}" && extra_dir_grub="${script_dir}/local/grub${extra_dir_grub}"
@@ -24,83 +26,34 @@ show_usage () {
  exit 100
 }
 
-log () {
- local msg="$@"
- echo "$msg"
-}
-
-check_errors () {
- local status="$?"
- local msg="$@"
- if [ "$status" != "0" ]; then
-  if [ "z$msg" != "z" ]; then
-   log "$msg"
-  else
-   log "ERROR: last operation finished with error code $status"
-  fi
-  exit $status
- fi
-}
-
 #destination dir
 efibase="$@"
-test -z "${efibase}" && show_usage
-
-test -d "${efibase}"
-check_errors "destination dir is not exist"
-
-test -d "${source_dir_shim}"
-check_errors "source_dir_shim ${source_dir_shim} is not exist"
+[[ -z "${efibase}" ]] && show_usage
+[[ ! -d "${efibase}" ]] && echo "destination dir is not exist" && exit 1
+[[ ! -d "${source_dir_shim}" ]] && echo "source_dir_shim ${source_dir_shim} is not exist" && exit 2
 
 efidir="${efibase}/EFI/BOOT"
-
 #cleanup old installations
 rm -rf "${efibase}/boot"
-check_errors "failed to remove ${efibase}/boot dir"
-
 rm -rf "${efibase}/BOOT"
-check_errors "failed to remove ${efibase}/BOOT dir"
-
 rm -rf "${efidir}"
-check_errors "failed to remove ${efidir} dir"
-
 #create dir and install required binaries
 mkdir -p "${efidir}"
-check_errors "failed to create ${efidir} dir"
-
 cp "${source_dir_shim}/shim.efi" "${efidir}/boot${efi_arch}.efi"
-check_errors
-
 cp "${source_dir_shim}/shim"*.der "${efidir}"
-check_errors
-
 cp "${source_dir_shim}/MokManager.efi" "${efidir}"
-check_errors
-
 cp "${script_dir}/grub-secure.cfg" "${efidir}/grub.cfg"
-check_errors
-
 sed -i "s|__vendor__|BOOT|g" "${efidir}/grub.cfg"
-check_errors
 
-if [ "z${grub_override_arch}" = "zy" ]; then
+if [[ "${grub_override_arch}" = "y" ]]; then
  cp "${source_dir_grub}/grub.efi" "${efidir}"
- check_errors
  cp "${source_dir_grub}/grub.der" "${efidir}"
- check_errors
 else
  cp "${source_dir_grub}/grub.efi" "${efidir}/grub${efi_arch}.efi"
- check_errors
  cp "${source_dir_grub}/grub.der" "${efidir}/grub${efi_arch}.der"
- check_errors
 fi
 
 cp "${extra_dir_grub}/unicode.pf2" "${efidir}"
-check_errors
 
 #copy local public keys
-if [ -d "${script_dir}/keys" ]; then
- cp "${script_dir}/keys/"*.der "${efidir}"
- check_errors
-fi
-
+[[ -d "${script_dir}/keys" ]] && cp "${script_dir}/keys/"*.der "${efidir}"
