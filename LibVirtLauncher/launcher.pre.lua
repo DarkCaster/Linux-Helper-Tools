1b790c08592213631cdcf73e53895f8392b4b4ec
DarkCaster <dark.caster@outlook.com>
2018-02-25 01:55:24 +0300

    LibVirtLauncher: launcher.pre.lua updated

---
+++ LibVirtLauncher/launcher.pre.lua
@@ -0,0 +1,8 @@
+loader.config={}
+loader.config.profile=loader.extra[1]
+loader.config.home_dir=loader.extra[2]
+loader.config.script_dir=loader.extra[3]
+loader.config.cur_dir=loader.extra[4]
+loader.config.config_uid=loader.extra[5]
+loader.config.tmp_dir=loader.extra[6]
+loader.config.ctl_dir=loader.extra[7]
