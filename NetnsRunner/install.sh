6ad47da0a5f3deae6d2adbae40782065a0f83736
DarkCaster <dark.caster@outlook.com>
2019-06-13 23:47:35 +0300

    NetnsRunner: install.sh updated

--- NetnsRunner/install.sh
+++ NetnsRunner/install.sh
@@ -12,15 +12,15 @@ curdir="$( cd "$( dirname "$0" )" && pwd )"
 
 cp "$curdir"/netns-*.sh "$target/bin"
 [[ -f /etc/sudoers.d/netns-runner ]] && rm "/etc/sudoers.d/netns-runner"
-cp "$curdir/netns-runner.sudoers" "/etc/sudoers.d/netns-runner"
+cp "$curdir/netns-runner.sudoers" "/etc/sudoers.d/99-netns-runner"
 
 "$curdir/update_shebang.sh" "$target/bin/netns-runner.sh"
 "$curdir/update_shebang.sh" "$target/bin/netns-exec.sh"
 
 sed -i "s|__prefix__|""$target/bin""|g" "$target/bin/netns-runner.sh"
 sed -i "s|__prefix__|""$target/bin""|g" "$target/bin/netns-exec.sh"
-sed -i "s|__prefix__|""$target/bin""|g" "/etc/sudoers.d/netns-runner"
+sed -i "s|__prefix__|""$target/bin""|g" "/etc/sudoers.d/99-netns-runner"
 
 chmod 755 "$target/bin/netns-runner.sh"
 chmod 700 "$target/bin/netns-exec.sh"
-chmod 440 "/etc/sudoers.d/netns-runner"
+chmod 440 "/etc/sudoers.d/99-netns-runner"
