7e9d9ccf53a3253d00f611399b656ee4db8aceb8
DarkCaster <dark.caster@outlook.com>
2020-10-04 01:58:59 +0300

    Setup/00_ansible_key: scripts updated

--- Setup/00_ansible_key/00-generate-key.sh
+++ Setup/00_ansible_key/00-generate-key.sh
@@ -5,4 +5,7 @@ set -e
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 cd "$script_dir"
 
-ssh-keygen -b 521 -t ecdsa -f ssh_key
+# for generating on loder PEM format
+# ssh-keygen -t rsa -b 4096 -m PEM
+
+ssh-keygen -b 521 -t ecdsa -f ssh.key
